736d0d648f9ce79b1e2fd0432415efb92025b464
Deep Bhimani <deep.bhimani@shunyavkash.com>
2023-10-11 15:34:39 +0530

    updated focus in footer input

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,9 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.js('src/js/app.js', 'public/js')
-    .sass(`src/scss/main.scss`, `public/css`, {}, [
-        require("tailwindcss")
-    ])
-    // .copy('src/fonts', 'public/fonts')
+mix
+  .js("src/js/app.js", "public/js")
+  .sass(`src/scss/main.scss`, `public/css`, {}, [require("tailwindcss")]);
 
-    mix.options({
-        processCssUrls: false
-    }); 
+mix.options({
+  processCssUrls: false,
+});
