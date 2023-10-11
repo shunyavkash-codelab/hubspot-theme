21a0f20e579e115e76c2a40670de6ebc4a1ae093
Deep Bhimani <deep.bhimani@shunyavkash.com>
2023-10-11 16:21:22 +0530

    Removed fancybox & slick slider by npm

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,1 @@
 require("jquery");
-
-/* =========================================================================== */
-
-/* fancybox import */
-import { Fancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
-
-/* fancybox */
-Fancybox.bind("[data-fancybox]", {});
-Fancybox.bind("[data-fancybox=gallery]", {});
-Fancybox.bind("[data-fancybox=facilities]", {});
-Fancybox.bind("[data-fancybox=school]", {});
-Fancybox.bind("[data-fancybox=events]", {});
-Fancybox.bind("[data-fancybox=sports]", {});
-Fancybox.bind("[data-fancybox=teaching]", {});
